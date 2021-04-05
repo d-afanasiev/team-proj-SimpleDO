9ddd933e236b10b293968e8bdb943508ec876706
d-afanasiev <59284294+d-afanasiev@users.noreply.github.com>
2021-04-05 14:40:46 +0000

    Deploying to gh-pages from @ d-afanasiev/team-proj-SimpleDO@7db32482b0272c0f41612ffb2c13a50310b11585 🚀

--- src.7f70b1fd.js
+++ src.7f70b1fd.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/main.js");
 },{"./sass/main.scss":"clu1","./js/main.js":"d6sW"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-proj-SimpleDO/src.e039a595.js.map
+//# sourceMappingURL=/team-proj-SimpleDO/src.7f70b1fd.js.map

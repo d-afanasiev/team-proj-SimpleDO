8e11fd28091bb7e0f4ceb444757f5db36ad12783
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-04-06 19:53:48 +0300

    Fix anchor

--- src/js/main.js
+++ src/js/main.js
@@ -34,4 +34,13 @@ $(".header__container__menu__link").click(function() {
         scrollTop: destination
     }, 500);
     return false;
+});
+
+$(".hero__btn").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+        scrollTop: destination
+    }, 500);
+    return false;
  });
